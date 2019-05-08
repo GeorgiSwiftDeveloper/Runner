45e81e05c797566ae2d3e24d42e7cb58b5a58571
GeorgiSwiftDeveloper <malkhasyangeorge@gmail.com>
2019-05-08 13:30:48 -0700

    Initial Commit

---
+++ RunApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  RunApp
+//
+//  Created by Georgi Malkhasyan on 5/8/19.
+//  Copyright © 2019 Malkhasyan. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RunApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  RunApp
+//
+//  Created by Georgi Malkhasyan on 5/8/19.
+//  Copyright © 2019 Malkhasyan. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
