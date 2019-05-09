9a3bfe02b2bd085f228107a6a650fafb977bdcb0
GeorgiSwiftDeveloper <malkhasyangeorge@gmail.com>
2019-05-08 17:54:06 -0700

    Track Location

--- RunApp/Controller/CurrentRunVC.swift
+++ RunApp/Controller/CurrentRunVC.swift
@@ -7,14 +7,50 @@
 //
 
 import UIKit
+import  MapKit
+
+
 
 class CurrentRunVC: LocationVC {
 
     @IBOutlet weak var swipeBGImageView: UIImageView!
  
     @IBOutlet weak var sliderImageView: UIImageView!
+    @IBOutlet weak var durationLbl: UILabel!
+    @IBOutlet weak var praceLbl: UILabel!
+    @IBOutlet weak var distanceLbl: UILabel!
     
     
+    
+    var startLocation: CLLocation!
+    var lastLocation: CLLocation!
+    
+    var runDistance = 0.0
+    
+    override func viewWillAppear(_ animated: Bool) {
+        manager?.delegate = self
+        
+        manager?.distanceFilter = 10
+          startRun()
+        
+        
+    }
+
+    func startRun() {
+        manager?.startUpdatingLocation()
+        
+    }
+    
+    
+    func endRun() {
+        manager?.stopUpdatingLocation()
+    }
+    
+    
+    @IBAction func pauseBtnPressed(_ sender: Any) {
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -48,4 +84,23 @@ class CurrentRunVC: LocationVC {
     }
 
 
+}
+
+
+extension CurrentRunVC: CLLocationManagerDelegate  {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse  {
+            checkLocationAuthStatus()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if startLocation == nil {
+            startLocation = locations.first
+        }else  if let location = locations.last{
+            runDistance += lastLocation.distance(from: location)
+            distanceLbl.text = "\(runDistance)"
+        }
+        lastLocation = locations.last
+    }
 }
